292d0b4dc0d341c9e0f05b1d0218bb314c8ac4fe
Philipp Wurm <philipp.wurm@gepardec.com>
2020-10-23 11:22:01 +0200

    initial commit

---
+++ molecule/debian10/tests/test_dhcp.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_is_dhcp_installed(host):
+    package_dhcp = host.package('isc-dhcp-server')
+
+    assert package_dhcp.is_installed
